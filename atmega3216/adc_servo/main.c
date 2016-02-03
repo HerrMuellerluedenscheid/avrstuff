20354214476f8440f0dddb81df7bff2ffc0496a6
Marius Kriegerowski <marius@gfz-potsdam.de>
2016-02-03 15:33:49 +0000

    float to char array

--- atmega3216/adc_servo/main.c
+++ atmega3216/adc_servo/main.c
@@ -4,6 +4,7 @@
 #include "lcd.h"
 #include "pwm.h"
 #include "util.h"
+#include "math.h"
 #define F_CPU 1000000L
 
 void ADC_Init(void){
@@ -18,6 +19,47 @@ void ADC_Init(void){
     ADCSRA |= (1<<ADSC);
 }
 
+
+void reverse(char *str, int len){
+    int i=0, j=len-1;
+    char temp;
+    while (i<j){
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++, j--;
+    }
+}
+
+
+int intToStr(int x, char str[], int d){
+    int i = 0;
+    while (x){
+        str[i++] = (x%10) + '0';
+        x = x/10;
+    }
+
+    while (i<d){
+        str[i++] = '0';
+    }
+    reverse(str, i);
+    str[i] = '\0';
+    return i;
+}
+
+void ftoa(float n, char *res, int afterpoint){
+    int ipart = (int)n;
+
+    float fpart = n - (float)ipart;
+
+    int i = intToStr(ipart, res, 0);
+
+    if (afterpoint != 0){
+        res[i] = '.';
+        fpart = fpart * pow(10, afterpoint);
+        intToStr((int)fpart, res + i + 1, afterpoint);
+    }
+}
 
 int main (void) {
 
@@ -38,10 +80,13 @@ int main (void) {
     _delay_ms(1000);
     lcd_write_instructions_4d(lcd_SetCursor | lcd_LineTwo);
     lcd_write_string_4d("XXXXXXXXXXXXXXXX");
-    //uint8_t poti_value;
     
-    float poti_value;
+    float poti_value, last_poti_value;
     float frac;
+    
+    lcd_write_instructions_4d(lcd_Clear);
+    lcd_write_instructions_4d(lcd_SetCursor | lcd_LineOne);
+    char res[20];
     while (1) {
         
         _delay_ms(100);
@@ -51,13 +96,14 @@ int main (void) {
         poti_value = ADCL;
         poti_value += (ADCH<<8);
         
-        //lcd_write_instructions_4d(lcd_Clear);
-        //lcd_write_instructions_4d(lcd_SetCursor | lcd_LineOne);
-        //lcd_write_string_4d(int(poti_value));
-        
         frac = (float) poti_value/1023.;
         set_PWM_range(frac, 0., 19999);
+        if (last_poti_value-poti_value!=0){
+            ftoa(poti_value, res, 1);
+            lcd_print(res);
+            last_poti_value = poti_value;
         }
+    }
     return 0;
 }
 
